0c188079b060b7ce58dad441c26a230873470b67
Lee-I-Lun <s655081@gmail.com>
2017-10-04 13:32:50 +0800

    Update utNumber.h

--- utNumber.h
+++ utNumber.h
@@ -8,12 +8,12 @@ TEST(Number, first_test){
 
 TEST(Number, call_value_method){
     Number one(1);
-    ASSERT_EQ(1,one.value());
+    ASSERT_EQ("1",one.value());
 }
 
 TEST(Number, call_symbol_method){
     Number ten(10);
-    ASSERT_EQ(10,ten.symbol());
+    ASSERT_EQ("10",ten.symbol());
 }
 
 TEST(Number, call_match_method) {
@@ -25,11 +25,11 @@ TEST(Number, call_match_method) {
 TEST(Number, match_and_then_test_value_symbol) {
   Number one(1);
   Number two(2);
-  EXPECT_EQ(1,one.value());
-  EXPECT_EQ(1,one.symbol());
+  EXPECT_EQ("1",one.value());
+  EXPECT_EQ("1",one.symbol());
   EXPECT_FALSE(one.match(two));
-  EXPECT_EQ(1,one.value());
-  ASSERT_EQ(1,one.symbol());
+  EXPECT_EQ("1",one.value());
+  ASSERT_EQ("1",one.symbol());
 }
 
 #endif
