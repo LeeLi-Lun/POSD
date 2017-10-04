d93f39c74852f89e3be870f69bc1dd8c2359798a
Lee-I-Lun <s655081@gmail.com>
2017-10-04 13:13:26 +0800

    Update variable.cpp

--- variable.cpp
+++ variable.cpp
@@ -2,40 +2,40 @@
 #include "number.h"
 #include "atom.h"
 
-bool Variable::match(Variable &a, Atom &b){
+bool Variable::match( Atom &b){
   bool isMatch = true;
-  if(a.value().length()==0 ){
-    a._value = b.value();
+  if(this.value().length()==0 ){
+    this._value = b.value();
   }
-  if( a.value() != b.value() ){
+  if( this.value() != b.value() ){
     isMatch = false;
   }
   return isMatch;
 }
 
-bool Variable::match(Variable &a, Number &b){
+bool Variable::match( Number &b){
   bool isMatch = true;
-  if(a.value().length()==0 ){
-    a._value = b.symbol();
+  if(this.value().length()==0 ){
+    this._value = b.symbol();
   }
-  if( a.value() != b.symbol() ){
+  if( this.value() != b.symbol() ){
     isMatch = false;
   }
   return isMatch;
 }
 
-bool Variable::match(Variable &a, Variable &b){
+bool Variable::match( Variable &b){
   bool isMatch = true;
-  if(a.value().length()==0  ){
-    a._value = b.value();
+  if(this.value().length()==0  ){
+    this._value = b.value();
   }
   if(b.value().length()==0 ){
-    b._value = a.value();
+    b._value = this.value();
   }
-  if( a.value() != b.value()){
+  if( this.value() != b.value()){
     isMatch = false;
   }
-  if(a.value().length()==0 && b.value().length()==0){
+  if(this.value().length()==0 && b.value().length()==0){
     isMatch = false;
   }
   return isMatch;
