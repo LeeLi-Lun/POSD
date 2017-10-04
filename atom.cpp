35aa63d7da11189cd22b8048d8d31a7c2659cb08
Lee-I-Lun <s655081@gmail.com>
2017-10-04 13:06:40 +0800

    Update atom.cpp

--- atom.cpp
+++ atom.cpp
@@ -1,30 +1,30 @@
 #include "atom.h"
 
-bool Atom::match(Atom &a, Atom &b){
+bool Atom::match(Atom &b){
   bool isMatch = true;
 
-  if( a.symbol() != b.symbol() ){
+  if( _symbol != b.symbol() ){
     isMatch = false;
   }else{
-    a._value = b.value();
+    _value = b.value();
   }
   return isMatch;
 }
 
-bool Atom::match(Atom &a, Number &b){
+bool Atom::match( Number &b){
   bool isMatch = true;
 
-  if( a.symbol() != b.symbol() ){
+  if( _symbol() != b.symbol() ){
     isMatch = false;
   }
   return isMatch;
 }
-bool Atom::match(Atom &b , Variable &a){
+bool Atom::match(Variable &a){
   bool isMatch = true;
   if(a.value().empty() ){
-      a._value = b.value();
+      a._value = this.value();
   }
-  if( a.value() != b.value() ){
+  if( a.value() != this.value() ){
     isMatch = false;
   }
   return isMatch;
