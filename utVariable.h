fbf192a5ff125c4a69502617f0d8f89610aea48a
Lee-I-Lun <s655081@gmail.com>
2017-10-04 13:19:21 +0800

    Update utVariable.h

--- utVariable.h
+++ utVariable.h
@@ -11,27 +11,27 @@ TEST(Variable, first_test){
 TEST (Variable , have_Atom_Value){
   Atom tom("tom");
   Variable X("X");
-  ASSERT_TRUE(X.match(X,tom));
+  ASSERT_TRUE(X.match(tom));
 }
 TEST (Variable , have_Number_Value){
   Number one(1);
   Variable X("X");
-  ASSERT_TRUE(X.match(X,one));
+  ASSERT_TRUE(X.match(one));
   ASSERT_EQ(X.value(),one.symbol());
 }
 TEST (Variable , match){
   Number one(1);
   Number two(2);
   Variable X("X");
-  EXPECT_TRUE(X.match(X,one));
-  ASSERT_FALSE(X.match(X,two));
+  EXPECT_TRUE(X.match(one));
+  ASSERT_FALSE(X.match(two));
 }
 TEST (Variable , match_variable){
   Number one(1);
   Variable X("X");
   Variable Y("Y");
-  EXPECT_TRUE(X.match(X,one));
-  ASSERT_TRUE(X.match(X,Y));
+  EXPECT_TRUE(X.match(one));
+  ASSERT_TRUE(X.match(Y));
 }
 
 #endif
