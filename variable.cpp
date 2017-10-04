13140bc0095e593de13102c624d6b7e5fce8a38d
Lee-I-Lun <s655081@gmail.com>
2017-10-04 13:26:18 +0800

    Update variable.cpp

--- variable.cpp
+++ variable.cpp
@@ -5,7 +5,7 @@
 bool Variable::match( Atom &b){
   bool isMatch = true;
   if(this.value().length()==0 ){
-    this._value = b.value();
+    this->_value = b.value();
   }
   if( this.value() != b.value() ){
     isMatch = false;
@@ -15,10 +15,10 @@ bool Variable::match( Atom &b){
 
 bool Variable::match( Number &b){
   bool isMatch = true;
-  if(this.value().length()==0 ){
-    this._value = b.symbol();
+  if(this->value().length()==0 ){
+    this->_value = b.symbol();
   }
-  if( this.value() != b.symbol() ){
+  if( this->value() != b.symbol() ){
     isMatch = false;
   }
   return isMatch;
@@ -26,16 +26,16 @@ bool Variable::match( Number &b){
 
 bool Variable::match( Variable &b){
   bool isMatch = true;
-  if(this.value().length()==0  ){
-    this._value = b.value();
+  if(this->value().length()==0  ){
+    this->_value = b.value();
   }
   if(b.value().length()==0 ){
-    b._value = this.value();
+    b._value = this->value();
   }
-  if( this.value() != b.value()){
+  if( this->value() != b.value()){
     isMatch = false;
   }
-  if(this.value().length()==0 && b.value().length()==0){
+  if(this->value().length()==0 && b.value().length()==0){
     isMatch = false;
   }
   return isMatch;
